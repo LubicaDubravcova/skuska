314a3c44ad7e372cb2cee9d10198482ce081cbde
Ľubica Dúbravcová <32597067+LubicaDubravcova@users.noreply.github.com>
2018-06-17 20:35:32 +0200

    cviko 9 uloha5 client

---
+++ SKcviko9U5client/src/skcviko9u5client/NewJerseyClient.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skcviko9u5client;
+
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.WebTarget;
+
+/**
+ * Jersey REST client generated for REST resource:GenericResource [generic]<br>
+ * USAGE:
+ * <pre>
+ *        NewJerseyClient client = new NewJerseyClient();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author Dell
+ */
+public class NewJerseyClient {
+
+    private WebTarget webTarget;
+    private Client client;
+    private static final String BASE_URI = "http://localhost:8080/SKcviko9U5/webresources";
+
+    public NewJerseyClient() {
+        client = javax.ws.rs.client.ClientBuilder.newClient();
+        webTarget = client.target(BASE_URI).path("generic");
+    }
+
+    public void deleteJedlo(String den, String index) throws ClientErrorException {
+        webTarget.path(java.text.MessageFormat.format("{0}/{1}", new Object[]{den, index})).request().delete();
+    }
+
+    public String getJedlo(String den, String index) throws ClientErrorException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("{0}/{1}", new Object[]{den, index}));
+        return resource.request(javax.ws.rs.core.MediaType.TEXT_PLAIN).get(String.class);
+    }
+
+    public <T> T getPocet(Class<T> responseType, String den) throws ClientErrorException {
+        WebTarget resource = webTarget;
+        resource = resource.path(java.text.MessageFormat.format("{0}", new Object[]{den}));
+        return resource.request(javax.ws.rs.core.MediaType.TEXT_PLAIN).get(responseType);
+    }
+
+    public void postJedlo(Object requestEntity, String den) throws ClientErrorException {
+        webTarget.path(java.text.MessageFormat.format("{0}", new Object[]{den})).request(javax.ws.rs.core.MediaType.TEXT_PLAIN).post(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.TEXT_PLAIN));
+    }
+
+    public void putJedlo(Object requestEntity, String den, String index) throws ClientErrorException {
+        webTarget.path(java.text.MessageFormat.format("{0}/{1}", new Object[]{den, index})).request(javax.ws.rs.core.MediaType.TEXT_PLAIN).put(javax.ws.rs.client.Entity.entity(requestEntity, javax.ws.rs.core.MediaType.TEXT_PLAIN));
+    }
+
+    public void close() {
+        client.close();
+    }
+    
+}
---
+++ SKcviko9U5client/src/skcviko9u5client/SKcviko9U5client.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package skcviko9u5client;
+
+/**
+ *
+ * @author Dell
+ */
+public class SKcviko9U5client {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        NewJerseyClient c = new NewJerseyClient();
+        System.out.println(c.getJedlo("Pon", "3"));
+        //c.deleteJedlo("Pon", "1");
+        //c.postJedlo("Kuracia polievka", "Pon");
+       // c.putJedlo("Granadir", "Pon", "3");
+    }
+    
+}
